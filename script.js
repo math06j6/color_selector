defbc39e5c918d9db7b8c68e734bd03bab569800
mathias <math06j6@stud.kea.dk>
2020-02-17 23:07:33 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,91 @@
+"use strict";
+
+
+
+window.addEventListener("DOMContentLoaded", start);
+
+const colorLabel = document.querySelector("#label");
+let picked;
+let defaultColor = "#0000ff";
+
+
+function start() {
+  console.log("start");
+
+  startup();
+}
+
+
+function startup() {
+
+
+  picked = document.querySelector("#picked");
+  picked.value = defaultColor;
+  picked.addEventListener("input", updateFirst, false);
+  picked.addEventListener("change", updateAll, false);
+  picked.select();
+}
+
+
+function updateFirst(event) {
+  const p = document.querySelector(".my-color");
+
+  if (p) {
+    p.style.background = event.target.value;
+  }
+}
+
+function updateAll(event) {
+  document.querySelectorAll("p").forEach(function(p) {
+    p.style.color = event.target.value;
+  });
+}
+
+function setColor() {
+	body.style.backgroundColor = input.value;
+	colorCode.innerHTML = input.value;
+}
+
+// function updateAll(event) {
+//   document.querySelectorAll("p").forEach(function(p) {
+//     p.style.color = event.target.value;
+//   });
+// }
+
+// function changeColor() {
+//   let h, s, l;
+ 
+//    const min = Math.min(r,g,b);
+//    const max = Math.max(r,g,b);
+  
+//    if( max === min ) {
+//      h = 0;
+//    } else
+//    if (max === r) {
+//      h = 60 * (0 + (g - b) / (max - min) );
+//    } else
+//    if (max === g) {
+//      h = 60 * (2 + (b - r) / (max - min) );
+//    } else
+//    if (max === b) {
+//      h = 60 * (4 + (r - g) / (max - min) );
+//    }
+  
+//    if (h < 0) {h = h + 360; }
+  
+//    l = (min + max) / 2;
+  
+//    if (max === 0 || min === 1 ) {
+//      s = 0;
+//    } else {
+//      s = (max - l) / ( Math.min(l,1-l));
+//    }
+//    // multiply s and l by 100 to get the value in percent, rather than [0,1]
+//    s *= 100;
+//    l *= 100;
+ 
+//    console.log("hsl(%f,%f%,%f%)", h, s, l);
+
+//   }
+
+
