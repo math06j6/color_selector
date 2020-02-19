2e61d4aa424fcf6a46c2227879f253d0677f14b3
mathias <math06j6@stud.kea.dk>
2020-02-19 10:36:58 +0100

    layout

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
 "use strict";
 
-
-
 window.addEventListener("DOMContentLoaded", start);
 
-
 let picked;
-let defaultColor = "#0000ff";
+let currentColor = "#0000ff";
 const HTML = {};
-
 
 function start() {
   console.log("start");
   HTML.colorLabel = document.querySelector("#label");
-
+  HTML.colorLabel = document.querySelector("#hexcode");
+  // const colorLabel = document.querySelector("#label");
   startup();
 }
 
-
 function startup() {
-
+  console.log("startup");
 
   picked = document.querySelector("#picked");
-  picked.value = defaultColor;
+  picked.value = currentColor;
   picked.addEventListener("input", updateFirst, false);
   picked.select();
 }
-
 
 function updateFirst(event) {
   const newColor = document.querySelector(".my-color");
@@ -37,56 +32,73 @@ function updateFirst(event) {
 }
 
 function setColor() {
-	body.style.backgroundColor = input.value;
-  colorCode.innerHTML = input.value;
-  
-  const colorCode = document.getElementById("colorCode");
+  console.log("setColor");
+
   colorCode.innerHTML = input.value;
 
- 
+  // const colorCode = document.querySelector("colorCode");
+
+  const colorCode = HTML.colorCode.value;
+
+  showHex(colorCode);
 }
 
+function bigFunction() {
+  const value = getValueFunction();
+  const converted = convertValueFunction(value);
+  displayValueFunction(converted);
+  const othervalue = convertAgain(converted);
+  displayOtherValue(otherValue);
+}
 
-// function updateAll(event) {
-//   document.querySelectorAll("p").forEach(function(p) {
-//     p.style.color = event.target.value;
-//   });
-// }
+//one of the convertValueFunctions could convert HEX (as a string) to RGB (as an object), and the corresponding display function would then display the RGB object on the page, maybe like textContent = `rgb( ${rgb.r}, ${rgb.g}, ${rgb.b})`;
 
-// function changeColor() {
-//   let h, s, l;
- 
-//    const min = Math.min(r,g,b);
-//    const max = Math.max(r,g,b);
-  
-//    if( max === min ) {
-//      h = 0;
-//    } else
-//    if (max === r) {
-//      h = 60 * (0 + (g - b) / (max - min) );
-//    } else
-//    if (max === g) {
-//      h = 60 * (2 + (b - r) / (max - min) );
-//    } else
-//    if (max === b) {
-//      h = 60 * (4 + (r - g) / (max - min) );
-//    }
-  
-//    if (h < 0) {h = h + 360; }
-  
-//    l = (min + max) / 2;
-  
-//    if (max === 0 || min === 1 ) {
-//      s = 0;
-//    } else {
-//      s = (max - l) / ( Math.min(l,1-l));
-//    }
-//    // multiply s and l by 100 to get the value in percent, rather than [0,1]
-//    s *= 100;
-//    l *= 100;
- 
-//    console.log("hsl(%f,%f%,%f%)", h, s, l);
+function convertValueFunction(a_value) {
+  // do some calculation
+  return a_convertedvalue;
+}
 
-//   }
+function convertAgain(a_different_value) {
+  // do some calculation
+  return a_new_kind_of_value;
+}
 
+function calcMyColors(r, g, b) {
+  console.log("calcMyColors");
 
+  r /= 255;
+  g /= 255;
+  b /= 255;
+
+  let h, s, l;
+
+  const min = Math.min(r, g, b);
+  const max = Math.max(r, g, b);
+
+  if (max === min) {
+    h = 0;
+  } else if (max === r) {
+    h = 60 * (0 + (g - b) / (max - min));
+  } else if (max === g) {
+    h = 60 * (2 + (b - r) / (max - min));
+  } else if (max === b) {
+    h = 60 * (4 + (r - g) / (max - min));
+  }
+
+  if (h < 0) {
+    h = h + 360;
+  }
+
+  l = (min + max) / 2;
+
+  if (max === 0 || min === 1) {
+    s = 0;
+  } else {
+    s = (max - l) / Math.min(l, 1 - l);
+  }
+  // multiply s and l by 100 to get the value in percent, rather than [0,1]
+  s *= 100;
+  l *= 100;
+
+  console.log("hsl(%f,%f%,%f%)", h, s, l);
+}
